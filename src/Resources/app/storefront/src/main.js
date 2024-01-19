3a440cfd6756b9bd717f3a32e4515bf9080d5152
DavidSchuchert <Davidschuchert@icloud.com>
2024-01-19 19:42:27 +0100

    made buttoncolor changeable true Admin page

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,18 +1,21 @@
-console.info("TEEST")
+console.info("TEEST");
 
-document.addEventListener("DOMContentLoaded", function() {
-    //select buy button
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.querySelectorAll("button.btn-buy");
 
-    buttons.forEach(function(button) {
-        button.addEventListener("click", function() {
-            // Set new button color
-            let originalColor = button.style.backgroundColor;
-            button.style.backgroundColor = "red"; // TestColor
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            let originalColor = button.style.backgroundColor; // Save initial button color
+            let originalText = button.textContent; // Save initial button text
 
-            //reset button color 
-            setTimeout(function() {
-                button.style.backgroundColor = originalColor;
+            // Set new button variables
+            button.classList.add("btn-cart-loading") // Use global variable
+            button.textContent = "test"; // Use global variable
+
+            // Reset button color and text
+            setTimeout(function () {
+                button.classList.remove("btn-cart-loading") // Use global variable
+                button.textContent = originalText;
             }, 1000);
         });
     });
