0e4cc889c41a9b855791af2fcb06600446586e67
DavidSchuchert <Davidschuchert@icloud.com>
2024-01-19 17:49:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# BuyButtonColorChange8Ways
---
+++ src/BuyButtonColorChange.php
@@ -0,0 +1,10 @@
+<?php declare(strict_types=1);
+
+namespace BuyButtonColorChange;
+
+use Shopware\Core\Framework\Plugin;
+use Shopware\Storefront\Framework\ThemeInterface;
+
+class BuyButtonColorChange extends Plugin implements ThemeInterface
+{
+}
