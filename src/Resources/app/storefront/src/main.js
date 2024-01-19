e27062bce60862542c3cb692863e7213379afb16
DavidSchuchert <Davidschuchert@icloud.com>
2024-01-19 18:08:13 +0100

    added color change functionality for the button

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,1 +1,19 @@
 console.info("TEEST")
+
+document.addEventListener("DOMContentLoaded", function() {
+    //select buy button
+    let buttons = document.querySelectorAll("button.btn-buy");
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function() {
+            // Set new button color
+            let originalColor = button.style.backgroundColor;
+            button.style.backgroundColor = "red"; // TestColor
+
+            //reset button color 
+            setTimeout(function() {
+                button.style.backgroundColor = originalColor;
+            }, 1000);
+        });
+    });
+});
