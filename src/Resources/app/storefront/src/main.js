ea2ed6ec4db79bb30095c8c060ebc37c1747306b
DavidSchuchert <Davidschuchert@icloud.com>
2024-01-19 20:04:22 +0100

    added button Color Textchange via Snippet

--- src/Resources/app/storefront/src/main.js
+++ src/Resources/app/storefront/src/main.js
@@ -1,20 +1,28 @@
-console.info("TEEST");
 
 document.addEventListener("DOMContentLoaded", function () {
+    // Select all button elements with the class 'btn-buy' and store them in 'buttons'.
     let buttons = document.querySelectorAll("button.btn-buy");
 
+    // Find the element with the ID 'buy-button-container' and store it in 'buyButtonContainer'.
+    let buyButtonContainer = document.getElementById("buy-button-container");
+
+    // Get the 'data-buy-confirmation-text' attribute from 'buyButtonContainer' to use later.
+    let buyConfirmationText = buyButtonContainer.getAttribute('data-buy-confirmation-text');
+
+    // Iterate over each button stored in 'buttons'.
     buttons.forEach(function (button) {
+        // Add a click event listener to each button.
         button.addEventListener("click", function () {
-            let originalColor = button.style.backgroundColor; // Save initial button color
-            let originalText = button.textContent; // Save initial button text
+            // Save the current text of the button for later use.
+            let originalText = button.textContent;
 
-            // Set new button variables
-            button.classList.add("btn-cart-loading") // Use global variable
-            button.textContent = "test"; // Use global variable
+            // Add a class to the button to show a loading state and change its text to the confirmation message.
+            button.classList.add("btn-cart-loading"); 
+            button.textContent = buyConfirmationText;
 
-            // Reset button color and text
+            // After 1 second, reset the button to its original state by removing the loading class and restoring the original text.
             setTimeout(function () {
-                button.classList.remove("btn-cart-loading") // Use global variable
+                button.classList.remove("btn-cart-loading");
                 button.textContent = originalText;
             }, 1000);
         });
